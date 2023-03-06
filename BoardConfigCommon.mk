805213a1ac023617472238624827bfa7ee64e573
Tim Zimmermann <tim@linux4.de>
2023-03-06 10:07:36 +0100

    exynos9820-common: Add basic f62 support
    Change-Id: I9309cfc41b2bef17aa605e56dfa05a6294f0ed6a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2022 The LineageOS Project
+# Copyright (C) 2021-2023 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,6 +21,17 @@ TARGET_SPECIFIC_HEADER_PATH := $(COMMON_PATH)/include
 ## Inherit proprietary vendor configuration
 include vendor/samsung/exynos9820-common/BoardConfigVendor.mk
 
+## Inherit platform specific configuration
+ifeq ($(TARGET_BOARD_PLATFORM), universal9825_r)
+include $(COMMON_PATH)/BoardConfig9825-r.mk
+include $(COMMON_PATH)/BoardConfig9825.mk
+else ifeq ($(filter beyond0lte beyond1lte beyond2lte beyondx,$(TARGET_DEVICE)),)
+include $(COMMON_PATH)/BoardConfig9825.mk
+include $(COMMON_PATH)/BoardConfig9820.mk
+else
+include $(COMMON_PATH)/BoardConfig9820.mk
+endif
+
 ## Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
@@ -40,7 +51,6 @@ BOARD_CUSTOM_BT_CONFIG := $(COMMON_PATH)/bluetooth/libbt_vndcfg.txt
 BOARD_HAVE_BLUETOOTH_BCM := true
 
 ## Boot Image
-BOARD_BOOTIMG_HEADER_VERSION := 1
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_OFFSET := 0x00008000
@@ -57,9 +67,6 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 
 ## Camera
 $(call soong_config_set,exynos9820CameraVars,exynos9820_model,$(TARGET_DEVICE))
-
-## DTB
-BOARD_PACK_RADIOIMAGES += dtb.img
 
 ## DTBO
 BOARD_KERNEL_SEPARATED_DTBO := true
@@ -93,15 +100,10 @@ DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
 ## Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 57671680
 BOARD_CACHEIMAGE_PARTITION_SIZE := 629145600
 BOARD_DTBIMG_PARTITION_SIZE := 8388608
 BOARD_DTBOIMG_PARTITION_SIZE := 8388608
 BOARD_FLASH_BLOCK_SIZE := 4096
-BOARD_PRODUCTIMAGE_PARTITION_SIZE := 650117120
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67633152
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 5976883200
-BOARD_VENDORIMAGE_PARTITION_SIZE := 1153433600
 
 BOARD_USES_METADATA_PARTITION := true
 
@@ -109,14 +111,6 @@ BOARD_ROOT_EXTRA_FOLDERS := efs
 
 ## Platform
 BOARD_VENDOR := samsung
-ifneq ($(filter beyond0lte beyond1lte beyond2lte beyondx,$(TARGET_DEVICE)),)
-TARGET_BOARD_PLATFORM := universal9820
-TARGET_BOOTLOADER_BOARD_NAME := exynos9820
-else
-DEVICE_MANIFEST_FILE += $(COMMON_PATH)/exynos9825_manifest.xml
-TARGET_BOARD_PLATFORM := universal9825
-TARGET_BOOTLOADER_BOARD_NAME := exynos9825
-endif
 TARGET_SOC := exynos9820
 include hardware/samsung_slsi-linaro/config/BoardConfig9820.mk
 
@@ -127,7 +121,6 @@ TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 ## Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
 BOARD_USES_FULL_RECOVERY_IMAGE := true
-TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/configs/init/fstab.exynos9820
 TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_exynos9820
 
@@ -149,8 +142,6 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 ## Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --include_descriptors_from_image $(PRODUCT_OUT)/dtb.img
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
 BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
