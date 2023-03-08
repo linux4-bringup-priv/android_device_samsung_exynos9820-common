85df298cc236a1718c96d265ee759af4bf611b0c
Tim Zimmermann <tim@linux4.de>
2023-03-08 11:04:37 +0100

    exynos9820-common: Set up dynamic partitions for exynos9825-r
    Change-Id: I6d717752f95605cc450717d99197d9cff589a5e0

--- common.mk
+++ common.mk
@@ -86,6 +86,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SamsungDoze
 
+# Dynamic Partitions
+ifeq ($(TARGET_BOARD_PLATFORM), universal9825_r)
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+endif
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor:32 \
